2d7a11a477199dd323b63436d9f5db514ad1d065
Blase Moylan <moylanblase@gmail.com>
2023-02-15 11:11:56 -0800

    Still in the process of encorporating additions and refactoring my code

--- RPG_project.py
+++ RPG_project.py
@@ -32,16 +32,70 @@ import random
     #  Remember, each function should do just one thing!  
 
 def run_Game():
+    #the hud
     ugly_green_dude_health=40
     your_health=25
     fight_is_over=False
+    crit_damage=10
+    crit_chance=10
+    block_chance=20
+    dodge_chance=10
+    attack_after_dodge=5
     story()
-    attack(fight_is_over,your_health,ugly_green_dude_health)
+    attack(fight_is_over,your_health,ugly_green_dude_health,crit_chance,block_chance,dodge_chance,attack_after_dodge,crit_damage)
     
 def story():
-    print("It was a dark day that saw Our Great kingdom brought to it's knees. Nature itself was revolting, creatures out of nightmares began to appear upon our sacred land. I remember that day well, though thoughts of our great kingdom's demise never once crossed my mind. For me The Fall all began one early summer morning,the sun was just cresting the nearby hill's when I woke to find a dagger attached to an ugly green arm, desending down upon me. (I must say this, impending death does wonders to get you out of bed in the morning). I quickly twisted the dagger out of the ugly arm's grip, and the fight of my life began... ")
+    print("It was a dark day that saw Our Great kingdom brought to it's knees. Nature itself was revolting, \ncreatures out of nightmares began to appear upon our sacred land. \nI remember that day well, though thoughts of our great kingdom's demise never once crossed my mind. \nFor me The Fall all began one early summer morning, the sun was just cresting the nearby hills \nwhen I woke to find a dagger attached to an ugly green arm, desending down upon me. \n(I must say this, impending death does wonders to get you out of bed in the morning). \nI quickly twisted the dagger out of the ugly arm's grip, and the fight of my life began... ")
 
-def attack(fight_is_over,your_health,ugly_green_dude_health):
+def norm_attack(num,crit_chance,block_chance,dodge_chance,attack_after_dodge):
+    norm=False
+    starting_num=attack_after_dodge+dodge_chance+block_chance
+    ending_num=101-crit_chance
+    if starting_num<num and num<ending_num:
+        norm=True
+        return norm
+    return norm
+
+def crit(crit_chance,num):
+    crit_hit=False
+    crit_chance=100-crit_chance
+    if num>crit_chance:
+        crit_hit=True
+        return crit_hit
+    return crit_hit
+
+def block(block_chance,num,dodge_chance,attack_after_dodge):
+    block=False
+    starting_num=attack_after_dodge+dodge_chance
+    ending_num=starting_num+block_chance+1
+    if starting_num<num and num<ending_num:
+        block=True
+        return block
+    return block
+
+def dodge(dodge_chance,num,attack_after_dodge):
+    dodge=False
+    starting_num=attack_after_dodge
+    ending_num=starting_num+dodge_chance+1
+    if starting_num<num and num<ending_num:
+        dodge=True
+        return dodge
+    return dodge
+
+#when implementing the attack after dodge need to add additional code in the attack function 
+# to let the user choose an attack
+
+
+def dodge_attack(attack_after_dodge,num):
+    attack=False
+    if num<attack_after_dodge+1:
+        attack=True
+        return attack
+    return attack
+
+    
+
+def attack(fight_is_over,your_health,ugly_green_dude_health,crit_chance,block_chance,dodge_chance,attack_after_dodge,crit_damage):
     attacks={'slash':5,'stab':15,'punch':1,'kick':2}
     list_of_attacks=attacks.keys()
     while fight_is_over==False:
@@ -51,8 +105,8 @@ def attack(fight_is_over,your_health,ugly_green_dude_health):
         num2=random.randrange(100)
         for item in list_of_attacks:
             if item==player_attack:
-                if num>80:
-                    ugly_green_dude_health=ugly_green_dude_health-attacks[item]-10
+                if crit(crit_chance,num):
+                    ugly_green_dude_health=ugly_green_dude_health-attacks[item]-crit_damage
                     print('crit')
                 elif 30<num and num<81:
                     ugly_green_dude_health=ugly_green_dude_health-attacks[item]
@@ -61,7 +115,7 @@ def attack(fight_is_over,your_health,ugly_green_dude_health):
                 elif num<11:
                     your_health=your_health-ugly_green_dude_attack
                     print('missed and took a hit')
-                print(f'ugly health:{ugly_green_dude_health}')
+                print(f'ugly health:{ugly_green_dude_health}')                
                 if num2>80:
                     your_health=your_health-ugly_green_dude_attack-10
                     print('took a crit')
@@ -75,10 +129,11 @@ def attack(fight_is_over,your_health,ugly_green_dude_health):
                 print(f'my health: {your_health}')          
                 # ugly_green_dude_health=ugly_green_dude_health-attacks[item]
                 if your_health<1:
-                    print("I saw it coming but I was helpless to stop it. And so I watched on in horror as the creature's claws tore into my chest, spilling the last of my life blood out onto the cold unforgiving floor. And thus my story ends, for I do not wish to speak of the horror that I have become.")
+                    print("I saw it coming but I was helpless to stop it. And so I watched on in horror as the creature's claws tore into my chest, \nspilling the last of my life blood out onto the cold unforgiving floor. \nAnd thus my story ends, for I do not wish to speak of the horror that I have become.")
                     fight_is_over=True
                 elif ugly_green_dude_health<1:
-                    print("At last there it was! An opening in uglie's gaurd. Without any hesitaion I plunged that dagger into it's throat, and watched on as life left it's eyes. After the thrill of the fight left me, I stood there for who knows how long, looking down upon the pool of blood that had gathered at my feet, with only one thing in my mind; a thought that still haunts me to this day, 'What have I become'. For I had enjoyed it. ")
+                    your_name=input('What is your name? ')
+                    print(f"At last there it was! An opening in uglie's gaurd. Without any hesitaion I plunged that dagger into it's throat, \nand watched on as life left it's eyes. After the thrill of the fight left me, \nI stood there for who knows how long, looking down upon the pool of blood that had gathered at my feet, \nwith only one thing in my mind; a thought that still haunts me to this day, \n'What have I become; what will I become!'. For I had enjoyed it.\nI am {your_name} and this is my Tale of the Dark Days.\nAm I Infamous or Legendary maybe both or just another casulty, who knows?\n but come back to the tale I digress. ")
                     fight_is_over=True
 
 # def actual_attack(your_health,ugly_green_dude_health):
